62e3019cdc416265e75b12ceb5eb0de34bf01efd
stdeguzman <seandeguzman0742@gmail.com>
2023-11-26 17:06:10 +0900

    no-frontend

--- frontend/my-app/src/Components/LoadingIndicator.jsx
+++
@@ -1,9 +0,0 @@
-import "../stylesheets/codeWindow.css";
-
-const LoadingIndicator = () => {
-	let loadingText = "Loading.....";
-
-	return <p>{loadingText}</p>;
-};
-
-export default LoadingIndicator;
